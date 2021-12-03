d42b354b90e486a6623797344821e9635f9a3c4a
s819229 <s81229@g.scvs.ntpc.edu.tw>
2021-12-03 11:12:25 +0800

    ok

--- library.py
+++ library.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app = Flask(raspkeylinebot)
+app = Flask(__name__)
 from mydb import get_book
 from flask import Flask, request, abort
 from linebot import  LineBotApi, WebhookHandler
